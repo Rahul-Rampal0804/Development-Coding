ac858a1506fbf0e6f930edafc3524b95efc4ecc5
Rahul Rampal <52612621+Rahul-Rampal0804@users.noreply.github.com>
2023-03-22 12:44:31 -0600

    Add files via upload

---
+++ househunting.java
@@ -0,0 +1,25 @@
+
+/*1. Title the program
+2. Explain what the program is doing
+3. State what kind of house you want(single family, duplex, condo, etc)
+4. What is the minimum price you would like to pay
+5. What is the maximum price you would like to pay
+6. How many bedrooms would you like
+7. How many bathrooms would you like
+8.Wwould you like to have a garage? Answer with a char (y or n)
+9. Should the house have a pool? Answer with a (boolean) true or false
+10. Where should the the house be located – what city
+11. Print out a report of the results */
+
+package javaclass;
+
+public class househunting
+{
+	public static void main(String [] args)
+	{
+		
+		
+		
+	}
+
+}
