5b0a6eeb21095962704e4820a0d6f83d7d749093
Rahul Rampal <52612621+Rahul-Rampal0804@users.noreply.github.com>
2023-05-13 19:09:58 -0600

    Add files via upload

---
+++ stringprint.java
@@ -0,0 +1,17 @@
+public class stringprint{
+public static void main(String a[]){
+   
+    String s;
+    int i=0;
+    int counter;
+    
+    
+    
+     counter=a.length;
+    System.out.println(a.length);
+    while(i<counter){
+     s=a[i];
+     i++;
+    
+System.out.println( s);    
+    }
